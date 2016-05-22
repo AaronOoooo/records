ed5ea60c79f6884dd80334b09418dff2fe4e5887
AaronOoooo <aaron.o.hall@gmail.com>
2016-05-21 20:39:37 -0500

    Add params and show

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,10 +1,52 @@
 class RecordsController < ApplicationController
-  
+
   def index
     @records = Record.all
+  end
+
+  def show
+    @record = Record.find(params[:id])
+  end
+
+  def many
+    @records = Record.all
+  end
+
+  def new
+
+  end
+
+  def create
+    record = Record.create(
+      artist: params[:artist],
+      cd_title: params[:cd_title],
+      year: params[:year],
+      record_label: params[:record_label],
+      cost: params[:cost]
+    )
+    redirect_to '/'
   end
 
   def first
     @record = Record.first
   end
+
+  def query
+    @message = params[:message]
+    @second_message = params[:ice_cream].upcase
+  end
+
+  def a_name
+    @my_name = params[:name]
+    if @my_name.first == 'a'
+      @begins_with_a = true
+    else
+      @begins_with_a = false
+    end
+  end
+
+  def url
+    @message = params[:penguins]
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   get '/index' => 'records#index'
   get '/first' => 'records#first'
 
-  get '/query_params_exampe' => 'records#single'
+  get '/query_params_example' => 'records#query'
+  get '/starts_with_an_a' => 'records#a_name'
+  get '/url_params_example/:penguins' => 'records#url'
+  get '/show/:id' => 'records#show'
+
+
+  get '/new' => 'records#new'
+  post '/records' => 'records#create'
 
 end
